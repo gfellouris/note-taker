b226aa8b12d3ecaae0c15217ef075fa59bb427f8
geofell <geofell22@gmail.com>
2019-08-06 23:41:09 -0400

    moved port and express server var

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require("express");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
 var PORT = process.env.PORT || 3000;
+var app = express();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
