1ccccb9f2d96fc8047cbf9f6d24fbe7a85c22676
geofell <geofell22@gmail.com>
2019-08-06 22:05:48 -0400

    added folder structure under public, assets: images javascript css

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,78 @@
+var faTrash = '<i class="fas fa-trash-alt float-right text-danger">';
+var faNotes = '<i class="fas fa-sticky-note text-info"></i>';
+
+var liStartNoteTitle = '<li class="list-group-item"> ';
+liStartNoteTitle += '<span class="font-weight-bold">';
+liStartNoteTitle += '<i class="fas fa-sticky-note text-info"></i>';
+var liEndNoteTitle = "</span>";
+var liStartNoteBody = "<p>";
+var liEndNoteBody = "</p></li>";
+
+$.get("/api/allnotes", function(data) {
+  console.log(data);
+  if (data) {
+    $("#noteInfo").show();
+    for (var i = 0; i < data.length; i++) {
+      var delItem =
+        "<i id=" +
+        data[i].id +
+        ' class="fas fa-trash-alt float-right text-danger"></i>';
+      var noteTitle =
+        liStartNoteTitle + data[i].title + liEndNoteTitle + delItem;
+      noteTitle += liStartNoteBody + data[i].body + liEndNoteBody;
+      $("#notesInfo").append(noteTitle);
+    }
+  } else {
+    $("#noteInfo").text(
+      "The force is not strong with this one. Your character was not found."
+    );
+    $("#notesInfo").hide();
+  }
+});
+
+$.get("/api/countnotes", function(data) {
+  console.log(data);
+  if (data) {
+    var countNotes = data[0].numnotes;
+    $("#countnotes").text(countNotes);
+  } else {
+    $("#noteInfo").text(
+      "The force is not strong with this one. Your character was not found."
+    );
+    $("#notesInfo").hide();
+  }
+});
+
+$(document).on("click", ".fa-trash-alt", function() {
+  //   var topicSelected = $(this).attr("data-name");
+  // alert($(this).attr("data-name"));
+  var delNoteId = $(this).attr("id");
+
+  $.get("/api/delete/" + delNoteId, function(data) {
+    console.log(data);
+    if (data) {
+      alert("Note " + delNoteId + " deleted.");
+    } else {
+      alert("Note " + delNoteId + " not found.");
+    }
+    location.reload();
+  });
+});
+
+$("#save-btn").on("click", function(event) {
+  event.preventDefault();
+  var newNote = {
+    title: $("#noteTitle")
+      .val()
+      .trim(),
+    body: $("#noteBody")
+      .val()
+      .trim()
+  };
+
+  $.post("/api/add", newNote).then(function(data) {
+    console.log("add.html", data);
+    // alert("Adding note...");
+    location.reload();
+  });
+});
